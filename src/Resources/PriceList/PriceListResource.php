228f9faec703265a0e5acda4fe86e319440e3f83
jeffersongoncalves <gerson.simao.92@gmail.com>
2026-06-30 19:05:13 +0000

    P3: admin depth — stats widget, image upload, order infolist + action (v5)

--- src/Resources/PriceList/PriceListResource.php
+++ src/Resources/PriceList/PriceListResource.php
@@ -2,8 +2,8 @@
 
 namespace JeffersonGoncalves\FilamentCommerce\Pricing\Resources\PriceList;
 
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use JeffersonGoncalves\Commerce\Pricing\Models\PriceList;
 use JeffersonGoncalves\FilamentCommerce\Pricing\CommercePricingPlugin;
@@ -26,9 +26,9 @@ class PriceListResource extends Resource
         }
     }
 
-    public static function form(Form $form): Form
+    public static function form(Schema $schema): Schema
     {
-        return PriceListForm::configure($form);
+        return PriceListForm::configure($schema);
     }
 
     public static function table(Table $table): Table
